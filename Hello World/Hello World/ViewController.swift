ec8fa52ffdd51df865f8b6b408e4acfa5b1907f9
vanessawyn <yananw2@student.unimelb.edu.au>
2017-10-02 10:08:29 +1100

    Implemented Say Functionality & Code Cleanup

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -14,13 +14,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Hello World")
-        print("Hello!")
+        say("Hello World")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func say(_ text: String){
+        print(text)
     }
 
 
